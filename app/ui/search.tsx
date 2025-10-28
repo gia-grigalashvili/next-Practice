ba1d24c173f8823f56d3735303bfcab6443419ab
gia-grigalashvili <grigalagia07@gmail.com>
2025-10-29 00:36:42 +0400

    start mutation data

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,18 +1,21 @@
 "use client";
 
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { useDebouncedCallback } from "use-debounce";
 import { useSearchParams, usePathname, useRouter } from "next/navigation";
 export default function Search({ placeholder }: { placeholder: string }) {
-  function handleSearch(term: string) {
-     console.log(`Searching... ${term}`);
+  const handleSearch = useDebouncedCallback((term) => {
+    console.log(`Searching... ${term}`);
+
     const params = new URLSearchParams(searchParams);
+    params.set("page", "1");
     if (term) {
       params.set("query", term);
     } else {
       params.delete("query");
     }
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
   //params.toString() targmnis momxrabelistvis tu ragaq shevyanili urlshi
   const pathname = usePathname();
   const { replace } = useRouter();
